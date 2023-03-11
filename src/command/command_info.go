d120703fad38786b4dda3e45b1fd2bacbac7c27c
domi41 <dominique.merle.41@gmail.com>
2023-03-11 11:37:42 +0100

    feat: add the `/mj info` command basics
    Only the quotas left are displayed for now, because that's the really important information.
    
    /spend 10m

---
+++ src/command/command_info.go
@@ -0,0 +1,76 @@
+package command
+
+import (
+	"fmt"
+	"github.com/andersfylling/disgord"
+	"github.com/sarulabs/di"
+	"log"
+	"main/src/container"
+	db "main/src/database"
+	"xorm.io/xorm"
+)
+
+type InfoCommand struct {
+	orm *xorm.Engine
+}
+
+func (c InfoCommand) Define() *disgord.ApplicationCommandOption {
+	return &disgord.ApplicationCommandOption{
+		Name:        "info",
+		Description: "Display miscellaneous information about this bot on this server",
+		Type:        disgord.OptionTypeSubCommand,
+	}
+}
+
+func (c InfoCommand) Matches(command string) bool {
+	return command == "info"
+}
+
+func (c InfoCommand) Handle(input Input) (handled bool, err error) {
+	if d, ok := (input).(DiscordInput); ok {
+		return true, handleInfoCommand(c, d)
+	}
+	return false, fmt.Errorf("unknown vendor")
+}
+
+func handleInfoCommand(
+	command InfoCommand,
+	input DiscordInput,
+) error {
+	guildVendorId, _ := input.GetGuildVendorId()
+	guild, err := db.GetOrCreateGuild(command.orm, guildVendorId)
+	if err != nil {
+		message := "Could not access the guild.  _Suddenly, everything is on fire. 🔥_"
+		return RespondCommandUserError(input, message)
+	}
+
+	err = input.Session.SendInteractionResponse(input.Context, input.Interaction, &disgord.CreateInteractionResponse{
+		Type: disgord.InteractionCallbackChannelMessageWithSource,
+		Data: &disgord.CreateInteractionResponseData{
+			Flags: disgord.MessageFlagEphemeral,
+			Content: "" +
+				"🤖🗩 _Here is some information about myself on this server._\n" +
+				"\n" +
+				"Polls remaining: " + fmt.Sprintf("%d", guild.Quota) +
+				"\n" +
+				"",
+		},
+	})
+
+	return err
+}
+
+func init() {
+	err := container.GetBuilder().Add(di.Def{
+		Name: "command.info",
+		Build: func(ctn di.Container) (interface{}, error) {
+			cmd := &InfoCommand{
+				orm: ctn.Get("database.engine").(*xorm.Engine),
+			}
+			return cmd, nil
+		},
+	})
+	if err != nil {
+		log.Fatalln("command.info failed to build", err)
+	}
+}
