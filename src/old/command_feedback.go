4fe732c2f2f170c4a0b4ebaca1cb24a3f19704b4
domi41 <dominique.merle.41@gmail.com>
2026-05-23 09:05:35 +0200

    feat: review the config before deployment
    /spend 45m

--- src/old/command_feedback.go
+++ src/old/command_feedback.go
@@ -1,33 +1,26 @@
 package old
 
-import (
-	//"github.com/andersfylling/disgord"
-	"github.com/sarulabs/di/v2"
-	"log"
-	"main/src/container"
-	db "main/src/database"
-	"main/src/domain"
-	"main/src/provider"
-	"xorm.io/xorm"
-)
-
-const FeedbackCommandSlug = "feedback"
-
-type FeedbackCommand struct {
-	orm *xorm.Engine
-}
-
-func (c FeedbackCommand) GetEmote() string {
-	return "💡"
-}
-
-func (c FeedbackCommand) GetName() string {
-	return FeedbackCommandSlug
-}
-
-func (c FeedbackCommand) GetDescription() string {
-	return "Send a message to my creators"
-}
+//import (
+//	"xorm.io/xorm"
+//)
+//
+//const FeedbackCommandSlug = "feedback"
+//
+//type FeedbackCommand struct {
+//	orm *xorm.Engine
+//}
+//
+//func (c FeedbackCommand) GetEmote() string {
+//	return "💡"
+//}
+//
+//func (c FeedbackCommand) GetName() string {
+//	return FeedbackCommandSlug
+//}
+//
+//func (c FeedbackCommand) GetDescription() string {
+//	return "Send a message to my creators"
+//}
 
 //func (c FeedbackCommand) DefineForDiscord() *disgord.ApplicationCommandOption {
 //	return &disgord.ApplicationCommandOption{
@@ -45,67 +38,67 @@ func (c FeedbackCommand) GetDescription() string {
 //	}
 //}
 
-func (c FeedbackCommand) Matches(command string) bool {
-	return command == FeedbackCommandSlug
-}
-
-func (c FeedbackCommand) Handle(input provider.Input) (handled bool, err error) {
-	return true, handleFeedbackCommand(c.orm, input)
-}
-
-func handleFeedbackCommand(orm *xorm.Engine, input provider.Input) error {
-
-	messageSent, _ := input.GetOptionString(FeedbackCommandSlug, "message", "")
-	if messageSent == "" {
-		message := "Please provide a message with your feedback."
-		return domain.RespondUserError(input, message)
-	}
-
-	guildVendorId, _ := input.GetGuildVendorId()
-	guild, err := db.GetGuild(orm, guildVendorId)
-	if err != nil {
-		message := "This community never held any poll.  Feedback requires experience."
-		return domain.RespondUserError(input, message)
-	}
-
-	actorVendorId, err := input.GetActorVendorId()
-	if err != nil {
-		message := "I cannot figure out who you are.  Feedback was canceled."
-		return domain.RespondUserError(input, message)
-	}
-	actorName, _ := input.GetActorName()
-
-	feedback := &db.Feedback{
-		GuildId:        guild.Id,
-		Content:        messageSent,
-		AuthorVendorId: actorVendorId,
-		AuthorName:     actorName,
-	}
-	_, err = orm.InsertOne(feedback)
-	if err != nil {
-		message := "I cannot write into my memory anymore.  Please contact my creator in any other way."
-		return domain.RespondServerError(input, message)
-	}
-
-	message := "🤖🗩 _Your feedback was successfully recorded.  **Thank you !**_" +
-		"\n" +
-		""
-
-	return provider.GetResponder(input).RespondMessage(input, message, true)
-}
-
-func init() {
-	err := container.GetBuilder().Add(di.Def{
-		Name: "command." + FeedbackCommandSlug,
-		Build: func(ctn di.Container) (interface{}, error) {
-			cmd := &FeedbackCommand{
-				orm: ctn.Get("database.engine").(*xorm.Engine),
-			}
-			return cmd, nil
-		},
-	})
-
-	if err != nil {
-		log.Fatalf("command.%s failed to build : %s\n", FeedbackCommandSlug, err)
-	}
-}
+//func (c FeedbackCommand) Matches(command string) bool {
+//	return command == FeedbackCommandSlug
+//}
+//
+//func (c FeedbackCommand) Handle(input provider.Input) (handled bool, err error) {
+//	return true, handleFeedbackCommand(c.orm, input)
+//}
+//
+//func handleFeedbackCommand(orm *xorm.Engine, input provider.Input) error {
+//
+//	messageSent, _ := input.GetOptionString(FeedbackCommandSlug, "message", "")
+//	if messageSent == "" {
+//		message := "Please provide a message with your feedback."
+//		return domain.RespondUserError(input, message)
+//	}
+//
+//	guildVendorId, _ := input.GetGuildVendorId()
+//	guild, err := db.GetGuild(orm, guildVendorId)
+//	if err != nil {
+//		message := "This community never held any poll.  Feedback requires experience."
+//		return domain.RespondUserError(input, message)
+//	}
+//
+//	actorVendorId, err := input.GetActorVendorId()
+//	if err != nil {
+//		message := "I cannot figure out who you are.  Feedback was canceled."
+//		return domain.RespondUserError(input, message)
+//	}
+//	actorName, _ := input.GetActorName()
+//
+//	feedback := &db.Feedback{
+//		GuildId:        guild.Id,
+//		Content:        messageSent,
+//		AuthorVendorId: actorVendorId,
+//		AuthorName:     actorName,
+//	}
+//	_, err = orm.InsertOne(feedback)
+//	if err != nil {
+//		message := "I cannot write into my memory anymore.  Please contact my creator in any other way."
+//		return domain.RespondServerError(input, message)
+//	}
+//
+//	message := "🤖🗩 _Your feedback was successfully recorded.  **Thank you !**_" +
+//		"\n" +
+//		""
+//
+//	return provider.GetResponder(input).RespondMessage(input, message, true)
+//}
+//
+//func init() {
+//	err := container.GetBuilder().Add(di.Def{
+//		Name: "command." + FeedbackCommandSlug,
+//		Build: func(ctn di.Container) (interface{}, error) {
+//			cmd := &FeedbackCommand{
+//				orm: ctn.Get("database.engine").(*xorm.Engine),
+//			}
+//			return cmd, nil
+//		},
+//	})
+//
+//	if err != nil {
+//		log.Fatalf("command.%s failed to build : %s\n", FeedbackCommandSlug, err)
+//	}
+//}
